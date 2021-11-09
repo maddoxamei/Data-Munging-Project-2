f6421a7f1429b0438e1ad95c0a39b26d19da865b
Mileva Van Tuyl <mvantuyl@wellesley.edu>
2021-11-09 16:31:07 -0500

    Child mortality and maternal health data

--- R/scripts/data-aquisition.R
+++ R/scripts/data-aquisition.R
@@ -2,7 +2,12 @@ get.dataset <- function(){
   ids <- list(gini="1TOQiiau_U3Zmf7UckYdp0vX9r0Hk6OSj", 
               poverty="1NGhqxQ9yG4rHBjHabUsEB0k4NjCpiRPg", 
               education_boys="1XivWARyORXko0MAnYPSnf5IIu0s8S72p", 
-              education_girls="1h43Tk-l0KlxTBMhcFpeSIYPTkU3jorld")
+              education_girls="1h43Tk-l0KlxTBMhcFpeSIYPTkU3jorld",
+              babies_per_woman="1CAdLSh8oehf4CaUHZc1Ax48XtjGkuFub",
+              infant_mortality="1pfT85eqVajo2g7qma3ivt73g7VIuSf0Y",
+              child_mortality="1EMRPXPjymLdShvCRn8LYBt5ckvt5iBfi",
+              maternal_mortality="1QQaI_YsMa68jnf3Tpcle9qanEH8nOZ1-",
+              income_per_person="1hfXAyRDE6HXr5CvGHv3fV0vph3ozDEwf")
   x <- lapply(ids, function(x){
     data <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", x))
     a <- grep("2000", colnames(data))
@@ -12,6 +17,3 @@ get.dataset <- function(){
   })
   return(x)
 }
-
-
-
