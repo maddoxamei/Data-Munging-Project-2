543a2729f5f3b47183c1bd66a95ee773613333aa
Mei Maddox <maddoxamei@gmail.com>
2021-11-15 11:16:45 -0500

    Add explicit package names

--- R/scripts/data-aquisition.R
+++ R/scripts/data-aquisition.R
@@ -28,8 +28,8 @@ get.melted.dataset <- function(data.list){
     purrr::reduce(dplyr::full_join) %>% 
     dplyr::mutate_at(c("year"), 
                      function(col) as.numeric(sub("X", "", col))) %>%
-    filter_at(vars(-one_of(c("country", "year"))),
-              any_vars(!is.na(.))) %>%
+    dplyr::filter_at(dplyr::vars(-tidyselect::one_of(c("country", "year"))),
+              dplyr::any_vars(!is.na(.))) %>%
     dplyr::mutate_at(c("country"), as.factor)%>%
     tibble::as_tibble()
 }
